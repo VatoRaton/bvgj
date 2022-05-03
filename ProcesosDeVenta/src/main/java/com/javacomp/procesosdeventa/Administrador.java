2089e209c6f0496d9c402005001904162697652d
Héctor Valverde <Hctor Valverde@LAPTOP-RGTUNA8A>
2022-05-03 15:54:01 +0200

    inicio

---
+++ ProcesosDeVenta/src/main/java/com/javacomp/procesosdeventa/Administrador.java
@@ -0,0 +1,34 @@
+package com.javacomp.procesosdeventa;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Administrador {
+    
+    private String correo;
+    private String clave;
+
+    public Administrador(String correo, String clave) {
+        this.correo = correo;
+        this.clave = clave;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+}
---
+++ ProcesosDeVenta/src/main/java/com/javacomp/procesosdeventa/Empresa.java
@@ -0,0 +1,37 @@
+package com.javacomp.procesosdeventa;
+
+public class Empresa extends Persona {
+    
+    private String CIF;
+    private String web;
+
+    public Empresa(String CIF, String web, String nombre, String correo,
+            String clave, Tarjeta tarjetaCredito, Direccion dir, int numeroTelf) {
+        super(nombre, correo, clave, tarjetaCredito, dir, numeroTelf);
+        this.CIF = CIF;
+        this.web = web;
+    }
+
+    public String getCIF() {
+        return CIF;
+    }
+
+    public void setCIF(String CIF) {
+        this.CIF = CIF;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "CIF=" + CIF + ", web=" + web + '}';
+    }
+    
+    
+}
